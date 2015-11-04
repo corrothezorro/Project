e12d31b61c6de0bd8542378b4747b17021b63793
Alex Boven <aboven@hotmail.de>
2015-11-04 14:19:13 +0100

    Komparator-Klassen

---
+++ Projekt/src/Comperators/DateComparator.java
@@ -0,0 +1,13 @@
+package Comperators;
+
+import java.util.Comparator;
+import java.util.Date;
+
+class DateComparator implements Comparator<Date>
+{
+    public int compare(Date date1, Date date2) 
+    {
+
+            return date1.compareTo(date2);
+    }
+}
---
+++ Projekt/src/Comperators/IntegerComparator.java
@@ -0,0 +1,14 @@
+package Comperators;
+
+import java.util.Comparator;
+
+
+public class IntegerComparator implements Comparator<String>{
+
+	public int compare(String num1, String num2) 
+    {
+
+            return Integer.parseInt(num1)-Integer.parseInt(num2);
+    }
+
+}
